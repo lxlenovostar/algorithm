cd3b61e2c6396af3c0243cb88a0295f2692c4369
lxlenovostar <lxlenovostar@gmail.com>
2016-06-15 08:02:00 +0800

    解答

--- leetcode/Nim_Game/main.c
+++ leetcode/Nim_Game/main.c
@@ -10,21 +10,7 @@ typedef int bool;
 #define false 0
 
 bool canWinNim(int n) {
-    if (n <= 3 && n >= 0)
-        return true;
-    else if (n == 4)
-        return false;
-    else {
-        //针对自身和对手的最优策略
-        //if (n - 1 == 0 || n - 2 <= 0 || n -3 <= 0) //对手
-        if (n >= 3)
-            canWinNim(n - 3);
-        else if (n >= 2)
-            canWinNim(n - 2);
-        else 
-            canWinNim(n - 1);
-    } 
-    return false;    
+    return (n % 4 != 0);
 }
 
 int main(void)
