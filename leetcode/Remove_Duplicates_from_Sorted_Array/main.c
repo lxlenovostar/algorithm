11a4dc562ddb5c0faa1fd7d0f201c80b9e6590bf
lxlenovostar <lxlenovostar@gmail.com>
2016-06-16 18:17:22 +0800

    解决

--- leetcode/Remove_Duplicates_from_Sorted_Array/main.c
+++ leetcode/Remove_Duplicates_from_Sorted_Array/main.c
@@ -8,7 +8,7 @@
 /*
  参数：一个数组， 数组长度
  目标: 删除数组中重复的元素
- *(nums + numsSize - 1 - i)
+ 条件：已经排序好的数组
 */
 int removeDuplicates(int* nums, int numsSize) {
     int i, k;
@@ -17,17 +17,21 @@ int removeDuplicates(int* nums, int numsSize) {
 
     for (i = 0; i < numsSize; i++) {
         /*
-         * 和已经确认的数据集进行比较
+         * 和已经确认的数据集进行比较, 因为是已经排序好的，所以可以少比较有很多。
          */
         k = j;
-        flag = 1;
+        
+        /*
         while(k > 0) {
-            if (nums[i] == nums[k-1])
+            if (nums[i] == nums[k-1]) {
                 flag = 0;
+                break;
+            }
             k--;
         }
-
-        if (flag == 0)
+        */
+         
+        if (k > 0 && nums[i] == nums[k-1]) 
             continue;
 
         nums[j] = nums[i];
@@ -42,10 +46,12 @@ void main(void)
     int nums[2] = {1, 2};
     int nums2[3] = {1, 1, 2};
     int nums1[5] = {3, 2, 1, 1, 2};
+    int nums3[4] = {0, 0, 0, 0};
     int i;
     printf("length is:%d\n", removeDuplicates(nums, 2)); 
     printf("length is:%d\n", removeDuplicates(nums2, 3)); 
     printf("length is:%d\n", removeDuplicates(nums1, 5)); 
+    printf("length is:%d\n", removeDuplicates(nums3, 4)); 
 
     for (i = 0; i < 2; i++)
         printf("array is:%d\n", nums[i]);
@@ -59,4 +65,9 @@ void main(void)
     
     for (i = 0; i < 5; i++)
         printf("array is:%d\n", nums1[i]);
+    
+    printf("\n");
+    
+    for (i = 0; i < 4; i++)
+        printf("array is:%d\n", nums3[i]);
 }
