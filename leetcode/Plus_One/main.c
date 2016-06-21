422458c9a289506e708aa5fb9f7b884cc5ca5da8
lxlenovostar <lxlenovostar@gmail.com>
2016-06-21 12:33:16 +0800

    调试大数存在BUG

--- leetcode/Plus_One/main.c
+++ leetcode/Plus_One/main.c
@@ -16,38 +16,76 @@
  */
 int* plusOne(int* digits, int digitsSize, int* returnSize) {
     int i;
-    int num = 0;
-    int ary = 1;
+    long long num = 0;
+    long long ary = 1;
     int *result;
 
-    for (i = digitsSize - 1; i >= 0; i++) {
+    printf("what0\n");
+    for (i = digitsSize - 1; i >= 0; i--) {
        num += digits[i] * ary;
        ary *= 10;
     }  
 
+    printf("what1\n");
     num += 1;
-    if (num%ary > 0) {
+    printf("num is:%ld, ary is:%d\n", num, ary);
+    if (num/ary > 0) {
         *returnSize = digitsSize + 1;
     }
     else {
         *returnSize = digitsSize;
     }   
 
+    printf("what2\n");
     result = (int *)malloc((*returnSize)*sizeof(int));
 
     ary = 1;    
-    for (i = 0; i < (*returnSize) - 1; i++) {
+    for (i = (*returnSize) - 1; i >= 0; i--) {
         result[i] = num % 10;
         num /= 10;   
     }  
 
+    printf("what3\n");
     return result;
 }
 
 
 void main(void)
 {
-    int size = 0;
+    int i;
     int testcase1[3] = {1, 2, 3};
+    int size1 = 0;
+    int *result1;
     int testcase2[4] = {9, 9, 9, 9};
+    int size2 = 0;
+    int *result2;
+    int testcase3[] = {7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,6};
+    int size3 = 0;
+    int *result3;
+    /*
+    result1 = plusOne(testcase1, 3, &size1);
+    printf("size is:%d\n", size1);
+    for (i = 0; i < size1; i++) {
+        printf("%d:", result1[i]);
+    }
+    printf("\n");
+    free(result1);
+
+    result2 = plusOne(testcase2, 4, &size2);
+    printf("size is:%d\n", size2);
+    for (i = 0; i < size2; i++) {
+        printf("%d:", result2[i]);
+    }
+    printf("\n");
+    free(result2);
+    */ 
+    
+    printf("size array is:%d\n", sizeof(testcase3)/sizeof(testcase3[0]));
+    result3 = plusOne(testcase3, sizeof(testcase3)/sizeof(testcase3[0]), &size3);
+    printf("size is:%d\n", size3);
+    for (i = 0; i < size3; i++) {
+        printf("%d:", result3[i]);
+    }
+    printf("\n");
+    free(result3);
 }
