50554c44317999f2ce396244bcce7df2d23b8056
lxlenovostar <lxlenovostar@gmail.com>
2016-09-01 18:10:17 +0800

    新题目：two-sum

---
+++ leetcode/Two_Sum/Solution.java
@@ -0,0 +1,28 @@
+/*
+ * 1.Two Sum.
+ *
+ */
+
+public class Solution {
+    public static int[] twoSum(int[] nums, int target) {
+        
+    }
+
+    public static void main(String[] args)
+    {
+        int i;
+
+        int[] a1 = new int[6];
+		a1[0] = 1;
+		a1[1] = 2;
+		a1[2] = 3;
+        int[] a2 = new int[3];
+		a2[0] = 4;
+		a2[1] = 5;
+		a2[2] = 6;
+
+        for (i = 0; i < 6; i++)
+            System.out.printf("%d:", a1[i]);       
+		System.out.println();       
+    }
+}
