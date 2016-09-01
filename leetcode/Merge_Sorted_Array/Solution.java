22db7927a0b34e86cceba6b78508407e4b624b63
lxlenovostar <lxlenovostar@gmail.com>
2016-09-01 18:08:24 +0800

    commit

--- leetcode/Merge_Sorted_Array/Solution.java
+++ leetcode/Merge_Sorted_Array/Solution.java
@@ -6,7 +6,7 @@
  */
 
 public class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
         int i, j, k;
         int[] aux = new int[m + n];
     
@@ -15,14 +15,34 @@ public class Solution {
         
         for (j = 0; j < n; j++)
             aux[i+j] = nums2[j];
+       
+		/* 
+		for (i = 0; i < 6; i++)
+            System.out.printf("%d:", aux[i]);       
+		System.out.println();       
+		*/
 
         i = 0;
         j = m;
         for (k = 0; k < (m + n); k++) {
-            if (i > m) nums1[k] = aux[j++]; 
-            else if (j > n) nums1[k] = aux[i++]; 
-            else if (aux[j] <= aux[i]) nums1[k] = aux[j++];
-            else nums1[k] = aux[j++];
+            if (i >= m) {
+				//System.out.println("fuck0");       
+				nums1[k] = aux[j++];
+			} 
+            else if (j >= (n+m)) {
+				//System.out.println("fuck1");       
+				nums1[k] = aux[i++]; 
+			}
+            else if (aux[j] <= aux[i]) {
+            	//System.out.printf("aux[i] %d, i %d, aux[j] %d, j %d", aux[i], i, aux[j], j);       
+				//System.out.println(" fuck2");       
+				nums1[k] = aux[j++];
+			}
+            else { 
+            	//System.out.printf("aux[i] %d, i %d, aux[j] %d, j %d", aux[i], i, aux[j], j);       
+				//System.out.println(" fuck3");       
+				nums1[k] = aux[i++];
+			}
         } 
     }
 
@@ -30,12 +50,19 @@ public class Solution {
     {
         int i;
 
-        int a1[6] = {1, 3, 5};
-        int a2[3] = {2, 4, 6};
+        int[] a1 = new int[6];
+		a1[0] = 1;
+		a1[1] = 2;
+		a1[2] = 3;
+        int[] a2 = new int[3];
+		a2[0] = 4;
+		a2[1] = 5;
+		a2[2] = 6;
     
-        merge(a1, 3, a2, 3);
+        Solution.merge(a1, 3, a2, 3);
 
         for (i = 0; i < 6; i++)
-            System.out.print("%d", a1[i]);       
+            System.out.printf("%d:", a1[i]);       
+		System.out.println();       
     }
 }
