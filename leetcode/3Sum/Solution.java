64b25b9b634ae8b375aef6e8ee1ccbab80ce60ce
lxlenovostar <lxlenovostar@gmail.com>
2016-09-05 17:24:46 +0800

    update it.

--- leetcode/3Sum/Solution.java
+++ leetcode/3Sum/Solution.java
@@ -7,43 +7,65 @@
  * 将时间复杂度从3次方下降到2次方。
  *
  *
- java 中如何用泛型
+ 0.看看 arraylist
+ 1.迭代器的使用 ？？
  */
+import java.util.*;
 
 public class Solution {
      public static List<List<Integer>> threeSum(int[] nums) {
-        HashMap<Integer, Integer [2]> map = new HashMap<>();
-
+        List<List<Integer>> threesum = new ArrayList<List<Integer>>();    
+        //List<List<Integer>> 3sum = new ArrayList<>();
+        HashMap<Integer, int[]> map = new HashMap<Integer, int[]>();
+        
         int[] temp; 
         for (int i = 0; i < nums.length; ++i) {
             for (int j = i+1; j < nums.length; ++j) {
-            if (map.get(nums[i] + nums[j]) != null) {
-                continue;
-            }
+                if (map.get(nums[i] + nums[j]) != null) {
+                    continue;
+                }
             temp = new int[2];
             temp[0] = nums[i];
             temp[1] = nums[j];
             map.put((nums[i] + nums[j]), temp);
+            }
         }
 
-       
+        for (int i = 0; i < nums.length; ++i) {
+            if (map.get(-nums[i]) != null) {
+                List<Integer> result = new ArrayList<Integer>();
+                result.add(nums[i]);
+                int[] value = map.get(-nums[i]);
+                result.add(value[0]);
+                result.add(value[1]);
+
+                threesum.add(result);
+            }
+        }
+
+        return threesum;
     }
-}
 
     public static void main(String[] args)
     {
-        int[] a1 = new int[4];
-		a1[0] = 2;
-		a1[1] = 7;
-		a1[2] = 11;
-		a1[3] = 15;
+        int[] a1 = new int[6];
+		a1[0] = -1;
+		a1[1] = 0;
+		a1[2] = 1;
+		a1[3] = 2;
+		a1[4] = -1;
+		a1[5] = -4;
     
-        int[] a2;
-        /*a2 = Solution.twoSum(a1, 9);
+        List<List<Integer>> result;
+        result = Solution.threeSum(a1);
 
-        for (int i = 0; i < 2; i++)
-            System.out.printf("%d:", a2[i]);       
-		System.out.println();       
-        */
+        for (Iterator<List<Integer>> iter = result.iterator(); iter.hashNext();) {
+            List<Integer> element = iter.next();
+            for (Iterator<Integer> inner_iter = element.iterator(); inner_iter.hashNext();) {
+                int e = inner_iter.next();
+                System.out.printf("%d:", e);       
+            }
+		    System.out.println();       
+        }
     }
 }
