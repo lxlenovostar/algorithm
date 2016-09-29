972cb2f0ccd72afbe291a480f38d3ca5990207c1
lxlenovostar <lxlenovostar@gmail.com>
2016-09-29 18:20:12 +0800

    需要再理清思路

--- leetcode/4Sum/Solution.java
+++ leetcode/4Sum/Solution.java
@@ -20,8 +20,9 @@ import java.util.*;
 public class Solution {
      public static List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> rst = new ArrayList<List<Integer>>();
-
-        if(nums == null || nums.length < 4) {
+		int min_intval = Integer.MAX_VALUE;
+        
+		if(nums == null || nums.length < 4) {
             return rst;
         }
 
@@ -31,7 +32,6 @@ public class Solution {
 			if (j != 0 && nums[j] == nums[j - 1]) {
 					continue; // to skip duplicate numbers; e.g [0,0,0,0]
 				}
-			int init_sum = nums[j] + nums[j+1] + nums[j+2] + nums[j+3];
 			for (int i = j + 1; i < nums.length - 2; i++) {
 					/*if (i != 0 && nums[i] == nums[i - 1]) {
 						continue; // to skip duplicate numbers; e.g [0,0,0,0]
@@ -40,42 +40,31 @@ public class Solution {
 					int left = i + 1;
 					int right = nums.length - 1;
 
-					while (left < right) {
-						int sum = nums[left] + nums[right] + nums[i] + nums[j];
+               		System.out.printf("j:%d i:%d left:%d right:%d\n", j, i, left, right);       
+               		System.out.printf("j_value:%d i_value:%d left_value:%d right_value:%d\n", nums[j], nums[i], nums[left], nums[right]);       
 
-                		//System.out.printf("j:%d i:%d left:%d right:%d\n", j, i, left, right);       
-                		//System.out.printf("j_value:%d i_value:%d left_value:%d right_value:%d\n", nums[j], nums[i], nums[left], nums[right]);       
+					 while (left < right) {
+                		int sum = nums[left] + nums[right] + nums[i] + nums[j];
 
-						if (sum == target) {
-							ArrayList<Integer> tmp = new ArrayList<Integer>();
-							tmp.add(nums[j]);
-							tmp.add(nums[i]);
-							tmp.add(nums[left]);
-							tmp.add(nums[right]);
-
-							if (rst.indexOf(tmp) == -1)
-								rst.add(tmp);
-
-							left++;
-							right--;
-
-							while (left < right && nums[left] == nums[left - 1]) { // to skip duplicates
-								left++;
-							}
-
-							while (left < right && nums[right] == nums[right + 1]) { // to skip duplicates
-								right--;
-							}
-
-						} else if (sum < init_sum) {
-							left++;
-							init_sum = sum;
-						} else {
-							right--;
-							init_sum = sum;
-						}
+                //System.out.printf("sum:%d i:%d i_value:%d left:%d left_value:%d rigth:%d right_value:%d\n", sum, i, nums[i], left, nums[left], right, nums[right]);       
+                		if (Math.abs(sum - target) <= min_intval) {
+                    		min_intval = Math.abs(sum - target);
+                    		result = sum;
+                		}   
+        
+                		if (sum >= target) { 
+                    		right--;
+                    		while (left < right && nums[right] == nums[right + 1]) { // to skip duplicates
+                        		right--;
+                    	}   
+                		} else {
+                    		left++;
+                    		while (left < right && nums[left] == nums[left - 1]) { // to skip duplicates
+                        		left++;
+                    		}   
+                		}   
 					}
-				}
+			}
 		}
 
         return rst;
@@ -85,12 +74,13 @@ public class Solution {
     {
         //int[] a1 = {-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6};
         //int[] a1 = {1, 0, -1, 0, -2, 2};
+        int[] a1 = {-2, -1, 0, 0, 1, 2};
         //int[] a1 = {0, 0, 0, 0, 0};
-        int[] a1 = {-3, -1, 0, 2, 4, 5};
+        //int[] a1 = {-3, -1, 0, 2, 4, 5};
 
 
         List<List<Integer>> result;
-        result = Solution.fourSum(a1, 2);
+        result = Solution.fourSum(a1, 0);
 
         for (List<Integer> element: result) { 
             for (int e: element) {
