299321004e16cf96dd41e9c9e1f5d0233ba470cc
lxlenovostar <lxlenovostar@gmail.com>
2016-09-29 18:05:41 +0800

    update

--- leetcode/4Sum/Solution.java
+++ leetcode/4Sum/Solution.java
@@ -31,16 +31,20 @@ public class Solution {
 			if (j != 0 && nums[j] == nums[j - 1]) {
 					continue; // to skip duplicate numbers; e.g [0,0,0,0]
 				}
+			int init_sum = nums[j] + nums[j+1] + nums[j+2] + nums[j+3];
 			for (int i = j + 1; i < nums.length - 2; i++) {
-				if (i != 0 && nums[i] == nums[i - 1]) {
+					/*if (i != 0 && nums[i] == nums[i - 1]) {
 						continue; // to skip duplicate numbers; e.g [0,0,0,0]
-					}
+					}*/
 
 					int left = i + 1;
 					int right = nums.length - 1;
 
 					while (left < right) {
 						int sum = nums[left] + nums[right] + nums[i] + nums[j];
+
+                		//System.out.printf("j:%d i:%d left:%d right:%d\n", j, i, left, right);       
+                		//System.out.printf("j_value:%d i_value:%d left_value:%d right_value:%d\n", nums[j], nums[i], nums[left], nums[right]);       
 
 						if (sum == target) {
 							ArrayList<Integer> tmp = new ArrayList<Integer>();
@@ -49,7 +53,8 @@ public class Solution {
 							tmp.add(nums[left]);
 							tmp.add(nums[right]);
 
-							rst.add(tmp);
+							if (rst.indexOf(tmp) == -1)
+								rst.add(tmp);
 
 							left++;
 							right--;
@@ -62,10 +67,12 @@ public class Solution {
 								right--;
 							}
 
-						} else if (sum < 0) {
+						} else if (sum < init_sum) {
 							left++;
+							init_sum = sum;
 						} else {
 							right--;
+							init_sum = sum;
 						}
 					}
 				}
@@ -77,11 +84,13 @@ public class Solution {
     public static void main(String[] args)
     {
         //int[] a1 = {-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6};
-        int[] a1 = {1, 0, -1, 0, -2, 2};
+        //int[] a1 = {1, 0, -1, 0, -2, 2};
+        //int[] a1 = {0, 0, 0, 0, 0};
+        int[] a1 = {-3, -1, 0, 2, 4, 5};
 
 
         List<List<Integer>> result;
-        result = Solution.fourSum(a1, 3);
+        result = Solution.fourSum(a1, 2);
 
         for (List<Integer> element: result) { 
             for (int e: element) {
