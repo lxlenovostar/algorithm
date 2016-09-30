1abd609c77574832d1750f8263878dc4f7bb0e05
lxlenovostar <lxlenovostar@gmail.com>
2016-09-30 14:26:31 +0800

    尝试用二分法实现

--- leetcode/Find_Minimum_in_Rotated_Sorted_Array/Solution.java
+++ leetcode/Find_Minimum_in_Rotated_Sorted_Array/Solution.java
@@ -14,24 +14,27 @@ import java.util.*;
 
 public class Solution {
 	public static int findMin(int[] nums) {
-        
+		int result = nums[0];
+
+		for(int i = 0; i < nums.length -1; i++) {
+			if (nums[i] > nums[i+1]) {
+				result = nums[i+1];
+				return result;		
+			}
+		}        
+
+		return result;
     }
 
     public static void main(String[] args)
     {
-        //int[] a1 = {-2, -1, 0, 0, 1, 2};
-        int[] a1 = {0, 0, 0, 0, 0};
-        //int[] a1 = {-3, -1, 0, 2, 4, 5};
+        //int[] a1 = {4, 5, 6, 7, 0, 1, 2};
+        int[] a1 = {4, 5, 6, 7, 7, 1, 2};
+        //int[] a1 = {3, 4, 5, 2, 1};
 
 
-        List<List<Integer>> result;
-        result = Solution.fourSum(a1, 0);
+        int result = Solution.findMin(a1);
 
-        for (List<Integer> element: result) { 
-            for (int e: element) {
-                System.out.printf("%d:", e);       
-            }
-		    System.out.println();       
-        }
+        System.out.printf("%d:\n", result);       
     }
 }
