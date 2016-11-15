50f6797212538f90d95efd85d2b6191f7083ec2e
lxlenovostar <lxlenovostar@gmail.com>
2016-11-15 18:02:18 +0800

    add

--- leetcode/Search_a_2D_Matrix/Solution.java
+++ leetcode/Search_a_2D_Matrix/Solution.java
@@ -33,16 +33,23 @@ public class Solution {
         }  else {
             int mid = length/2;
             
-            System.out.printf("mid:%d\n", mid);       
+            //System.out.printf("length:%d mid:%d\n", length, mid);       
 
             if (target == matrix[mid])
                 return true;
             else if (target > matrix[mid]) {
+                //System.out.printf("here1\n");       
                 int[] dest = new int[mid];
-                System.arraycopy(matrix, mid, dest, 0, mid);
+
+                if (mid+1 == length)
+                    System.arraycopy(matrix, mid, dest, 0, mid);
+                else 
+                    System.arraycopy(matrix, mid+1, dest, 0, mid);
+
                 flag = check_value(dest, target);
             }
             else { 
+                //System.out.printf("here2\n");       
                 int[] dest = new int[mid];
                 System.arraycopy(matrix, 0, dest, 0, mid);
                 flag = check_value(dest, target);
@@ -56,12 +63,13 @@ public class Solution {
         int row = matrix.length - 1;
         int col = matrix[0].length - 1;        
 	
-        System.out.printf("row:%d col:%d\n", row, col);       
+        //System.out.printf("row:%d col:%d\n", row, col);       
         if (target < matrix[0][0] || target > matrix[row][col])
             return false;
 
         for (int i = 0; i <= row; i++) {
             if (target <= matrix[i][col]) {
+                //System.out.printf("row:%d\n", row);       
                 return check_value(matrix[i], target);
             }
         }        
@@ -71,17 +79,16 @@ public class Solution {
 
     public static void main(String[] args)
     {
-        //int[] a = {2, 1, 5, 6, 2, 3};
-        //int[] a = {1};
-        //int[] a = {0};
         /*int [][] a = {
                 {1,   3,  5,  7},
                 {10, 11, 16, 20},
                 {23, 30, 34, 50}
                 };        
         */
-        int [][] a = { {1,   3,  5}};
+        //boolean result = Solution.searchMatrix(a, 5);
+        //boolean result = Solution.searchMatrix(a, 31);
 
+        int [][] a = { {1,   3,  5}};
         boolean result = Solution.searchMatrix(a, 5);
 
         System.out.printf("%d:\n", result == true ? 0: 1);       
