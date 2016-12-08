482419ad56e89b3c935e7fdf86f8bbc09db6ad76
lxlenovostar <lxlenovostar@gmail.com>
2016-12-08 17:48:33 +0800

    优秀实现

--- leetcode/Find_Peak_Element/Solution.java
+++ leetcode/Find_Peak_Element/Solution.java
@@ -64,6 +64,33 @@ public class Solution {
         return index;
     }
 
+	/* 优秀实现　*/
+	public int findPeakElement_right(int[] nums) {
+		int n = num.size();
+
+		if(n == 1) {
+			return 0;
+		} 
+
+		int start = 0;
+		int end = n - 1;
+		int mid = 0;
+
+		while(start <= end) {
+			mid = start + (end - start) / 2;
+
+			if((mid == 0 || num[mid] >= num[mid - 1]) && (mid == n - 1 || num[mid] >= num[mid + 1])) {
+				return mid;
+			} else if (mid > 0 && num[mid-1] > num[mid]) {
+				end = mid - 1;
+			} else {
+				start = mid + 1;
+			}
+		}
+
+		return mid;
+	}
+	
     public static void main(String[] args)
     {
         //int[] a =  {1, 2, 3, 1}; 
