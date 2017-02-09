6a5544d50ec9c32f54cbeba9b51758be39d7d670
lxlenovostar <lxlenovostar@gmail.com>
2017-02-09 11:14:37 +0800

    need debug

--- leetcode/Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal/Solution.java
+++ leetcode/Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal/Solution.java
@@ -102,6 +102,7 @@ public class Solution{
         return keys;
     }  
 
+	/* 迭代不用返回回来 */
 	private int get_maxdepth(TreeNode node, int depth)
 	{
 		int left_depth = depth + 1;
@@ -112,13 +113,9 @@ public class Solution{
 		}
 
 		left_depth = get_maxdepth(node.left, depth+1);	
-	
 		right_depth = get_maxdepth(node.right, depth+1);	
 
-		if (left_depth >= right_depth)
-			return left_depth;
-		else 
-			return right_depth;
+        return Math.max(left_depth, right_depth);
 	}
 
 	public int maxDepth() {
@@ -131,23 +128,106 @@ public class Solution{
             return 0;
         }
 
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
+        int left = maxDepth_1(root.left);
+        int right = maxDepth_1(root.right);
         return Math.max(left, right) + 1;
     }
 
-	public TreeNode buildTree(int[] inorder, int[] postorder) {
+	/* anthor way. */
+	private int depth;
+    
+    public int maxDepth_2(TreeNode root) {
+        depth = 0;
+        helper(root, 1);
         
+        return depth;
+    }
+    
+    private void helper(TreeNode node, int curtDepth) {
+        if (node == null) {
+            return;
+        }
+        
+        if (curtDepth > depth) {
+            depth = curtDepth;
+        }
+        
+        helper(node.left, curtDepth + 1);
+        helper(node.right, curtDepth + 1);
+    }	
+
+	private int findPosition(int[] src, int start, int end, int key) {
+		if (start < end || start < 0)
+			return -1;
+
+		for (int i = start; i <= end; i++) {
+			if (src[i] == key) {
+				return i;
+			}
+		} 
+
+		return -1;
+	}
+
+	private TreeNode help_BuildTree(int[] inorder, int instart, int inend, int[] postorder, int poststart, int postend) {
+		if (instart > inend || postend < 0) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(postorder[postend]);
+        int position = findPosition(inorder, instart, inend, postorder[postend]);
+
+		root.left = help_BuildTree(inorder, instart, position, postorder, poststart, poststart + (position - instart - 1)); 
+		root.right = help_BuildTree(inorder, position+1, inend, postorder, poststart + (position - instart - 1) + 1, postend - 1); 
+
+		return root;
+	}
+
+	public TreeNode buildTree(int[] inorder, int[] postorder) {
+		/*
+		 通过postorder可以把inorder切成左右子树，得到左子树的前序和后序，
+　　　　 然后
+		 */        
+		if (inorder.length != postorder.length) {
+            return null;
+        }
+
+        return help_BuildTree(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
+
+		/*int i, left_len, right_len;
+		int root = postorder[postorder.length -1];
+		this.put(root);
+
+		for(i = 0; i < inorder.length; i++) {
+			if (inorder[i] == root)
+				break;
+			else {
+				//BUG
+			}
+		}	
+
+		left_len = i + 1;
+		right_len = inorder.length - left_len - 1;
+
+		int[] inorder_left = new int[left_len];
+		System.arraycopy(inorder, );
+		int[] postorder_left = new int[left_len];
+
+		int[] inorder_right = new int[right_len];
+		int[] postorder_right = new int[right_len];
+
+		buildTree(inorder_left, postorder_left);
+		buildTree(inorder_right, postorder_right);
+		*/
     }
 	
     public static void main(String[] args)
     {
-		Solution so = new Solution();
 		int result;
-
-		so.put(0);
+		Solution so = new Solution();
 
 		/*
+		so.put(0);
 		so.put(2);
         System.out.printf("\n");       
 		so.put(3);
@@ -162,13 +242,16 @@ public class Solution{
         System.out.printf("%d\n", result);       
 		*/
 
+		int[] inorder = {4, 2, 5, 1, 6, 3, 7}; 
+		int[] postorder = {4, 5, 2, 6, 7, 3, 1};
+
+	 	so.root = so.buildTree(inorder, postorder);
+ 
         System.out.printf("\nnew begin\n");       
 		result = so.maxDepth();
         System.out.printf("maxdepth:%d\n", result);       
 		 
-		/*
 		for (int i : so.levelOrder())
         	System.out.printf("%d:", i);       
-		*/
     }
 }
