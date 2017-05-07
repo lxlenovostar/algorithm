ddd6cffba15a3b7dc631223b499c887cb9a8b54e
lxlenovostar <lxlenovostar@gmail.com>
2017-05-07 21:20:18 +0800

    finish 82

--- leetcode/82_Remove_Duplicates_from_Sorted_List_II/solution.cpp
+++ leetcode/82_Remove_Duplicates_from_Sorted_List_II/solution.cpp
@@ -6,7 +6,11 @@
  * For example,
  * Given 1->2->3->3->4->4->5, return 1->2->5.
  * Given 1->1->1->2->3, return 2->3.
- *
+ * 
+ * TODO:
+ * 1.如果head和head->next一样，删除head,pre->next等于head->next
+ * 2.如果head需要删除的head是真正的head, 那么result 就等于head->next
+ * 3.
  * */
 
 #include <iostream>
@@ -21,6 +25,58 @@
  
 class Solution {
 	public:
+		ListNode* deleteDuplicates(ListNode* head) {
+			if(head == NULL || head->next == NULL) {
+				return head;
+			} 
+
+			ListNode dummy(0);
+			dummy.next = head;
+			ListNode* prev = &dummy;
+			ListNode* p = head;
+
+			while(p && p->next) {
+				if (p->val == p->next->val) {
+					int val = p->val;
+					ListNode* tmp = p->next->next;
+
+					delete p->next;
+					delete p;
+					while (tmp) {
+						if (tmp->val != val)
+							break;
+
+
+						ListNode* need_d = tmp;
+						tmp = tmp->next;
+						delete need_d;
+					}
+					prev->next = tmp;
+					p = tmp;
+				} else {
+					prev = p;
+					p = p->next;
+				}
+			}
+			
+			/*
+			 * 处理返回值很棒!
+			 * */	
+			return dummy.next;
+		}
+		
+		void printList(ListNode* head) {
+			if(head == NULL) {
+				return ;
+			} 
+
+			std::cout << head->val << std::endl;	
+			while(head->next != NULL) {
+				head = head->next;
+				std::cout << head->val << std::endl;	
+			}
+		}
+	
 		void freeList(ListNode* head) {
 			if(head == NULL) {
 				return ;
@@ -36,60 +92,23 @@ class Solution {
 			}
 		}
 
-		ListNode* deleteDuplicates(ListNode* head) {
-			ListNode *result = head;
-			if(head == NULL || head->next == NULL) {
-				return result;
-			} 
-				
-			int head_value = head->val; 
-
-			while(head != NULL && head->next != NULL) {
-				ListNode* tmp = head->next;
-
-				if (head->val == tmp->val) {
-					if (tmp->val == head_value)
-						result = tmp;
-					
-					delete head;
-				} else {
-					if (head->val == head_value)
-						head_value = tmp->val;
-				} 
-					
-				head = tmp;
-			}
-				
-			return result;
-		}
-		
-		void printList(ListNode* head) {
-			if(head == NULL) {
-				return ;
-			} 
-
-			std::cout << head->val << std::endl;	
-			while(head->next != NULL) {
-				head = head->next;
-				std::cout << head->val << std::endl;	
-			}
-		}
-
 
 };
 
 int  main() {
-	ListNode *a = new ListNode(4);
-	ListNode *b = new ListNode(4);
+	ListNode *a = new ListNode(2);
+	ListNode *b = new ListNode(2);
 	b->next = a;
-	ListNode *c = new ListNode(2);
+	/*
+	ListNode *c = new ListNode(1);
 	c->next = b;
 	ListNode *d = new ListNode(1);
 	d->next = c;
+	*/
 
 	Solution *test = new Solution();
-	test->printList(d);
-	ListNode *head =  test->deleteDuplicates(d);	
+	test->printList(b);
+	ListNode *head =  test->deleteDuplicates(b);	
 	std::cout << "finish." << std::endl;
 	test->printList(head);
 	test->freeList(head);
