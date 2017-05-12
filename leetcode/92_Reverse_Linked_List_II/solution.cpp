f83cd130e3183535c26c40257b7f0470a6d22d4e
lxlenovostar <lxlenovostar@gmail.com>
2017-05-12 14:26:26 +0800

    update formate.

--- leetcode/92_Reverse_Linked_List_II/solution.cpp
+++ leetcode/92_Reverse_Linked_List_II/solution.cpp
@@ -5,7 +5,7 @@
  * For example:
  * Given 1->2->3->4->5->NULL, m = 2 and n = 4,
  * return 1->4->3->2->5->NULL.
- 
+ *
  * Note:
  * Given m, n satisfy the following condition:
  * 1 ≤ m ≤ n ≤ length of list.
