c033a804cc16094beff9962357d1439437ce9685
lxlenovostar <lxlenovostar@gmail.com>
2019-11-25 22:12:47 +0800

    1.fix it

--- leetcode/206_reverse_linked_list/reverse_linked_list.c
+++ leetcode/206_reverse_linked_list/reverse_linked_list.c
@@ -36,6 +36,51 @@ struct ListNode* reverseList(struct ListNode* head) {
 	return stack;
 }
 
+struct ListNode* reverseList2(struct ListNode* head) {
+	if (head == NULL || head->next == NULL) {
+		return NULL;
+	} else {
+		struct ListNode *p1 = head;
+		struct ListNode *p2 = head->next;
+		struct ListNode *p3 = NULL;
+
+		while (p2) {
+			p3 = p2->next;
+			p2->next = p1;
+			p1 = p2;
+			p2 = p3;
+		}
+
+		head->next = NULL;
+		return p1;
+	}	
+}
+
+struct ListNode* reverseList3(struct ListNode* head) {
+	if (head == NULL || head->next == NULL) {
+		return head;
+	} else {
+		struct ListNode* new_head = reverseList3(head->next);
+
+		head->next->next = head;
+		head->next = NULL;
+
+		return new_head;
+	}
+}
+
+/*
+struct ListNode* reverseList_2(struct ListNode* head) {
+		if (head == NULL)
+			return NULL;
+		else {
+			struct ListNode *stack = head;
+			reverseList2(head->next, stack);	
+			return stack;
+		}
+}
+*/
+
 void print_list(struct ListNode *head) {
 	printf("print begin\n");
 	while(head != NULL) {
@@ -80,7 +125,7 @@ int main()
 	a5->next = NULL;
 
 	print_list(a1);
-	struct ListNode *result = reverseList(a1);
+	struct ListNode *result = reverseList3(a1);
 	print_list(result);
 	free_list(result);
 
