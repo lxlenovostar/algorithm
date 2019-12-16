381bda82232a491b7f31f29756227a09dd7f5bc8
lxlenovostar <lxlenovostar@gmail.com>
2019-12-16 21:08:38 +0800

    1.fix it

---
+++ leetcode/242_isAnagram/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+		if (s.compare(t) == 0)
+			return true;
+
+		size_t len = s.size();
+		std::map<char, int> s_map;
+
+		for (size_t i = 0; i < len; i++) {
+			if (s_map.find(s[i]) == s_map.end()) {
+				s_map[s[i]] = 1;
+			} else {
+				s_map[s[i]] += 1;
+			
+			}
+		}
+
+		len = t.size();
+		for (size_t i = 0; i < len; i++) {
+			if (s_map.find(t[i]) == s_map.end()) {
+				return false;
+			} else {
+				s_map[t[i]] -= 1;
+
+				if (s_map[t[i]] == 0)
+					s_map.erase(t[i]);
+			}
+		}
+
+		if (s_map.size() == 0)
+			return true;
+		else 
+			return false;
+        
+    }
+};
+
+int main() {
+	Solution *ret = new Solution();
+
+	std::cout << ret->isAnagram("abc", "abc") << std::endl;
+	std::cout << ret->isAnagram("a", "b") << std::endl;
+	std::cout << ret->isAnagram("ab", "ba") << std::endl;
+	std::cout << ret->isAnagram("", "") << std::endl;
+
+	return 0;
+}
