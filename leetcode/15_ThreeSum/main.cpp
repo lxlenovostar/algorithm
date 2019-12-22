375fbeccb1e4ef4c6e6062f8b8caa6f7caa91c01
lxlenovostar <lxlenovostar@gmail.com>
2019-12-22 09:57:25 +0800

    1.don't use set

--- leetcode/15_ThreeSum/main.cpp
+++ leetcode/15_ThreeSum/main.cpp
@@ -23,7 +23,7 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
 		vector<vector<int>> ret;
-		set<vector<int>> set_ret;
+		//set<vector<int>> set_ret;
 
 		if (nums.empty())
 			return ret;
@@ -35,10 +35,13 @@ public:
 
 		for (; pos != nums.end(); pos++) {
 			if (pos != nums.begin()) {
+				std::cout << "1 new:" << *pos << " old:" << old << std::endl;
                 if (*pos == old) {
+					std::cout << "2 new:" << *pos << " old:" << old << std::endl;
                     old = *pos;
                     continue;
                 }
+				old = *pos;
             }
 
 			vector<int>::iterator first = pos + 1;
@@ -52,20 +55,34 @@ public:
 				if (value == 0) {
 					std::cout << "get it" << std::endl;
 					vector<int> elem = {*pos, *first, *end};
-					if (set_ret.find(elem) == set_ret.end())
-						set_ret.insert(elem);
+					//if (set_ret.find(elem) == set_ret.end())
+					//	set_ret.insert(elem);
+					ret.push_back(elem);
+
 					end--;
+					while (first < end && *end == *(end+1)) {
+						std::cout << "end--" << std::endl;
+						end--;
+					}
+
 					first++;
+					while (first < end && *first == *(first-1))  {
+						std::cout << "first++" << std::endl;
+						first++;
+					}
+
 				} else if (value > 0) {
+
 					end--;
 				} else {
+
 					first++;
 				}
 			}
 		}
 
-		for (set<vector<int>>::iterator it = set_ret.begin(); it != set_ret.end(); ++it)
-			ret.push_back(*it);
+		//for (set<vector<int>>::iterator it = set_ret.begin(); it != set_ret.end(); ++it)
+		//	ret.push_back(*it);
 
 		return ret;
 
@@ -80,7 +97,7 @@ int main() {
 	//vector<int> nums = {-1, 0, 1, 2, -1, -4};
 	//vector<int> nums = {14,4,6,-1,10,9,-8,7,-13,14,-13,-11,-8,-9,11,14,-8,-14,-13,7,-10,-15,-13,-11,-11,11,14,13,2,-14,1,-7,-2,14,-1,-15,9,7,-1,3,6,1,7,5,-1,-5,4,-2,-4,-1,-9,-7,-1,-7,-11,3,12,10,-7,-1,12,1,8,-13,1,14,9,-13,6,-7,-3,-11,2,-11,10,-14,-1,-9,0,2,5,6,3,-11,6,7,0,3,3,0,-12,-8,-13,3,-14,-5,2,10,-11,-14,-12,1,-10,5,5,7,-1,11,14,6,-10,-4,-3,8,-7,10,1,8,-1,-11,-15,-6,-12,-13,12,-11};
 	//vector<int> nums = {0, 0, 0};
-	vector<int> nums;
+	vector<int> nums = {-2,0,3,-1,4,0,3,4,1,1,1,-3,-5,4,0};
 
 	Solution *obj = new Solution();
 	vector<vector<int>> ret =  obj->threeSum(nums);
