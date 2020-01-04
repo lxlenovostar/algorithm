e3e31e7e9f413edee45ad186d963fb65674b2715
lxlenovostar <lxlenovostar@gmail.com>
2020-01-04 13:53:40 +0800

    1.fix it.

--- leetcode/169_majorityElement/main.cpp
+++ leetcode/169_majorityElement/main.cpp
@@ -46,11 +46,59 @@ public:
 
 		return max_val;
     }
+
+	int countInRange(vector<int>& nums, int num, int lo, int hi) {
+		int count = 0;
+
+		for (int i = lo; i <= hi; ++i) {
+			if (nums[i] == num)
+				count++;
+		}
+
+		return count;
+	}
+
+	int helper(vector<int>& nums, int lo, int hi) {
+        // base case; the only element in an array of size 1 is the majority
+        // element.
+        if (lo == hi) {
+			std::cout << "lo == hi: " << nums[lo] <<  " lo: " << lo << " hi: " << hi << std::endl;
+            return nums[lo];
+        }
+
+        // recurse on left and right halves of this slice.
+        int mid = (hi-lo)/2 + lo;
+		std::cout << "lo: " << lo << " mid: " << mid << " hi: " << hi << std::endl;
+		std::cout << "begin left" <<  " lo: " << lo << " hi: " << mid << std::endl;
+        int left = helper(nums, lo, mid);
+		std::cout << "begin right" <<  " lo: " << (mid+1) << " hi: " << hi << std::endl;
+        int right = helper(nums, mid+1, hi);
+
+        // if the two halves agree on the majority element, return it.
+        if (left == right) {
+            return left;
+        }
+
+        // otherwise, count each element and return the "winner".
+        int leftCount = countInRange(nums, left, lo, hi);
+        int rightCount = countInRange(nums, right, lo, hi);
+
+        int ret = leftCount > rightCount ? left : right;
+		std::cout << "ret: " << ret << " leftCount: " << leftCount << " rightCount: " << rightCount << " lo: " << lo << " hi: " << hi << std::endl;
+		return ret;
+    }
+
+
+    
+	int majorityElement_1(vector<int>& nums) {
+		return helper(nums, 0, nums.size()-1);
+	}
 };
 
 int main() {
 	Solution *obj = new Solution();
-	vector<int> nums = {2,2,1,1,1,2,2};
-	std::cout << obj->majorityElement(nums) << std::endl;
+	//vector<int> nums = {2,2,1,1,1,2,2};
+	vector<int> nums = {8,9,8,9,8};
+	std::cout << obj->majorityElement_1(nums) << std::endl;
 
 }
