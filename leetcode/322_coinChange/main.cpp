ba31cc3962f36ba9d6a5bd8eeb59c7ac36dfd7f8
lxlenovostar <lxlenovostar@gmail.com>
2020-01-28 12:48:51 +0800

    1.fix it.

--- leetcode/322_coinChange/main.cpp
+++ leetcode/322_coinChange/main.cpp
@@ -6,6 +6,7 @@
 输入: coins = [1, 2, 5], amount = 11
 输出: 3 
 解释: 11 = 5 + 5 + 1
+
 示例 2:
 
 输入: coins = [2], amount = 3
@@ -17,6 +18,7 @@
 #include <algorithm>
 #include <vector>
 #include <iostream>
+#include <limits.h>
 
 using namespace std;
 
@@ -24,7 +26,7 @@ class Solution {
 public:
 	
 	//gready 
-    int coinChange(vector<int>& coins, int amount) {
+    int coinChange_1(vector<int>& coins, int amount) {
 
 		//sort coins
 		sort(coins.begin(), coins.end(), greater<int>());
@@ -65,11 +67,48 @@ public:
 		else 
 			return -1;
     }
+    
+	int coinChange(vector<int>& coins, int amount) {
+		
+		//dp[i] = dp[i]
+		//
+		//dp[11] = min( dp[10] + 1, dp [9] + 2,  dp[6] + 5  )
+		//
+		//dp[i] = min(dp[i - 1] + 1, dp[i - 2] + 1, dp[i - 5] + 1)
+		//
+		//dp[0] = -1;
+		//dp[1] = 1;
+		//dp[2] = 1;
+		//dp[3] = 2;
+		//dp[4] = 2;
+		//dp[5] = 1;
+		//dp[6] = 2;
+
+		int Max = amount + 1;
+		int coins_len = coins.size();
+
+		vector<int> dp(amount+1, Max);
+		dp[0]  = 0;	
+
+		for (int i = 1; i <= amount; ++i) {
+			for (int j = 0; j < coins_len; ++j) {
+				std::cout << " j: " << i << " coins[j]: " << coins[j] << std::endl;
+				if (i >=  coins[j]) {
+					dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+				} // if
+
+			} // for 
+			
+			std::cout << " i: " << i << " dp[i]: " << dp[i] << std::endl;
+		}
+
+		return dp[amount] > amount ? -1 : dp[amount];
+	}
 };
 
 int main() {
 		//vector<int> coins = {1, 2, 5}; 
-		//int amount = 0;
+		//int amount = 11;
 		//vector<int> coins = {2}; 
 		//int amount = 3;
 		vector<int> coins = {186, 419, 83, 408}; 
@@ -79,3 +118,5 @@ int main() {
 		std::cout << obj->coinChange(coins, amount) << std::endl;
 		return 0;
 }
+
+
