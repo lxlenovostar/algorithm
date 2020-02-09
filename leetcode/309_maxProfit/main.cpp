09191d41e2538679daed6abe591a386b2ce2c60f
lxlenovostar <lxlenovostar@gmail.com>
2020-02-09 21:42:09 +0800

    1.fix it.

--- leetcode/309_maxProfit/main.cpp
+++ leetcode/309_maxProfit/main.cpp
@@ -21,7 +21,7 @@ using namespace std;
 
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
+    int maxProfit_1(vector<int>& prices) {
 		int n = prices.size();
 
 		if (n <= 1)
@@ -91,6 +91,35 @@ public:
 		return ret;
 
     }
+
+	//dp
+	int maxProfit(vector<int>& prices) {
+		int n = prices.size();
+
+		if (n <= 1)
+			return 0;
+
+		//dp[-1][k][0] = dp[i][0][0] = 0
+		//dp[-1][k][1] = dp[i][0][1] = -infinity
+		
+		//dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
+		//dp[i][1] = max(dp[i-1][1], dp[i-2][0] - prices[i])
+		
+		int dp_i_0 = 0;
+		int dp_i_1 = INT_MIN;
+		int dp_pre_0 = 0;
+
+		for (int i = 0; i < n; ++i) {
+			int temp = dp_i_0;
+			dp_i_0 = max(dp_i_0, dp_i_1 + prices[i]);
+			dp_i_1 = max(dp_i_1, dp_pre_0 - prices[i]);
+			dp_pre_0 = temp;
+		}
+
+		return dp_i_0;
+	}
+
+
 };
 
 int main() {
