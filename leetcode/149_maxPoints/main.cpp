46187ee3674ddefcd2ef6f00ad018996ebdcb13e
lxlenovostar <lxlenovostar@gmail.com>
2020-03-18 21:27:39 +0800

    1.need fix it.

--- leetcode/149_maxPoints/main.cpp
+++ leetcode/149_maxPoints/main.cpp
@@ -37,29 +37,45 @@ using namespace std;
 class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
-		if (points.size() <= 0)	
-			return 0;
+		if (points.size() <= 1)	
+			return points.size();
 
-		map<int, int> point_count;
 		int points_size = points.size();
-		for (int i = 0; i < points_size; ++i) {
-			int tmp;
-			if (points[i][1] != 0)
-		   		tmp = points[i][0]/points[i][1];
-			else 
-				tmp = 0;
-		
-			point_count[tmp]++;
-		}
-
 		int max_value = 0;
+		for (int i = 0; i < points_size; ++i) {
+			map<double, int> point_count;
 
-		for (auto it = point_count.begin(); it != point_count.end(); ++it)  {
-			if (it->second >= max_value)	
-				max_value = it->second;
-		}
+				double slope = 0.0f;
+				for (int j = i+1; j < points_size; ++j) {
+					double molecule    = points[j][1] - points[i][1];
+					double denominator = points[j][0] - points[i][0];
 
-		return max_value;
+					std::cout << "i: " << i << " j: " << j << std::endl;
+					std::cout << "y1: " << points[j][1] << " x1: " << points[j][0] << " y: " << points[i][1] << " x: " << points[i][0] << std::endl; 
+					std::cout << "molecule: " << molecule << " denominator: " << denominator << std::endl;
+
+					if (points[j][1] == points[i][1] && points[j][0] == points[i][0]) {
+						point_count[slope]++;
+						continue;
+					}
+
+					if (denominator != 0)
+		   				slope = molecule/denominator;
+		
+					point_count[slope]++;
+					
+					std::cout << "slope: " << slope << std::endl;
+				} //for j
+
+
+			for (auto it = point_count.begin(); it != point_count.end(); ++it)  {
+				if (it->second >= max_value)	
+					max_value = it->second;
+			} //for auto
+		} //for i
+
+
+		return (max_value+1);
     }
 };
 
@@ -72,6 +88,7 @@ int main() {
 	};
 	*/
 	
+	/*
 	vector<vector<int>> nums = {
 		{1, 1}, 
 		{3, 2}, 
@@ -80,7 +97,28 @@ int main() {
 		{2, 3},
 		{1, 4}
 	};
+	*/
 
+	/*
+	vector<vector<int>> nums = {
+		{0, 0}, 
+		{0, 0} 
+	};
+	*/
+	
+	/*
+	vector<vector<int>> nums = {
+		{0, 0}, 
+		{1, 1}, 
+		{0, 0}
+	};
+	*/
+
+	vector<vector<int>> nums = {
+		{1, 1}, 
+		{1, 1}, 
+		{2, 3}
+	};
 
 	Solution *objs = new Solution();
 	std::cout << "result: " << objs->maxPoints(nums) <<  std::endl;
