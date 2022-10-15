34419035569060ce120baf20be03bc612d0b4717
lxlenovostar <lxlenovostar@gmail.com>
2022-10-15 10:19:38 +0800

    fix(offer_ii 108)

--- leetcode/offer_ii_108/main_v2.cc
+++ leetcode/offer_ii_108/main_v2.cc
@@ -39,74 +39,44 @@ private:
 
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        std::queue<std::string> tmp_queue_begin;
-        std::queue<std::string> tmp_queue_end;
-        std::set<std::string> checked_node_begin; 
-        std::set<std::string> checked_node_end; 
-        std::queue<std::string> next_queue_begin;
-        std::queue<std::string> next_queue_end;
-
-        if (check_end(endWord, wordList) == false)
+        std::set<std::string> no_visited_node(wordList.begin(), wordList.end());
+       
+        if (no_visited_node.find(endWord) == no_visited_node.end()) {
             return 0;
+        }
+        
+        std::set<std::string> set_1;
+        std::set<std::string> set_2;
+        int length = 2;
 
-        tmp_queue_begin.push(beginWord);
-        tmp_queue_end.push(endWord);
-        int step = 2;
-        while (!tmp_queue_begin.empty() || !tmp_queue_end.empty()) {
+        set_1.insert(beginWord);
+        set_2.insert(endWord);
 
-            const auto& check_word = tmp_queue_begin.front();
-            tmp_queue_begin.pop();
-            if (check_word == endWord)
-                return step;
-
-            const auto &neighbors = get_neighbors(check_word, wordList);
-            for (const auto& word: neighbors) {
-                if (checked_node_begin.find(word) == checked_node_begin.end()) {
-                    std::cout << "what1 " << word <<  " step:" << step <<  std::endl;
-                    next_queue_begin.push(word);
-                    std::cout << "what3 " << word << "\n";
-                    checked_node_begin.insert(word);
-                }
-                if (checked_node_end.find(word) != checked_node_end.end()) {
-                    std::cout << "what6 " << "\n";
-                    return step+1;
-                }
+        while (!set_1.empty() && !set_2.empty()) {
+            // 从节点少的开始
+            if (set_2.size() < set_1.size()) {
+                std::swap(set_2, set_1);
             }
- 
-            const auto& check_end_word = tmp_queue_end.front();
-            tmp_queue_end.pop();
-            if (check_end_word == beginWord)
-                return step;
+        
+            std::set<std::string> set_3;
 
-            const auto &end_neighbors = get_neighbors(check_end_word, wordList);
-            for (const auto& word: end_neighbors) {
-                if (checked_node_end.find(word) == checked_node_end.end()) {
-                    std::cout << "what2 " << word <<  " step:" << step <<  std::endl;
-                    next_queue_end.push(word);
-                    std::cout << "what4 " << word << "\n";
-                    checked_node_end.insert(word);
-                }
-                if (checked_node_begin.find(word) != checked_node_begin.end()) {
-                    std::cout << "what7 " << "\n";
-                    return step+1;
-                }
+            for (const auto& item : set_1) {
+                const auto &neighbors = get_neighbors(item, wordList);
+                    for (const auto& word: neighbors) {
+                        if (set_2.find(word) != set_2.end()) {
+                            return length;
+                        }
+
+                        if (no_visited_node.find(word) != no_visited_node.end()) {
+                            no_visited_node.erase(word);
+                            set_3.insert(word);
+                        }
+
+                    }
             }
 
-            std::cout << "next begin: " << next_queue_begin.size() << std::endl;
-            std::cout << "next end: " << next_queue_end.size() << std::endl;
-   
-            if (tmp_queue_begin.empty()) {
-                std::swap(next_queue_begin, tmp_queue_begin);
-                ++step;
-            }
-
-            if (tmp_queue_end.empty()) {
-                std::swap(next_queue_end, tmp_queue_end);
-                ++step;
-            }
-            
-            std::cout << "tmp begin: " << tmp_queue_begin.size() << std::endl;
-            std::cout << "tmp end: " << tmp_queue_end.size() << std::endl;
+            std::swap(set_1, set_3);
+            ++length;
         }
 
         return 0;
@@ -114,19 +84,19 @@ public:
 };
 
 int main() {
-    /*
     std::string beginWord = "hit";
     std::string endWord = "cog";
     std::vector<std::string> wordList = {"hot", "dot", "dog", "lot", "log", "cog"};
 
+    /*
     std::string beginWord = "qa";
     std::string endWord = "sq";
     std::vector<std::string> wordList = {"si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar","ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"};
-    */
 
     std::string beginWord = "hit";
     std::string endWord = "cog";
     std::vector<std::string> wordList = {"hot","dot","dog","lot","log"};
+    */
 
     Solution *test = new Solution();
     std::cout << test->ladderLength(beginWord, endWord, wordList) << std::endl;
