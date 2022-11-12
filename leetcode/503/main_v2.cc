24198e6719cea5c9077273f63d1f5d931236ec64
lxlenovostar <lxlenovostar@gmail.com>
2022-11-12 15:34:13 +0800

    fix(503)

--- leetcode/503/main_v2.cc
+++ leetcode/503/main_v2.cc
@@ -1,35 +1,40 @@
 #include <stack>
 #include <vector>
 #include <iostream>
-#include <map>
 
 using namespace std;
 
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        stack<int> s;
+        std::vector<int> ret;    
+        ret.resize(nums.size(), -1);
+        std::stack<int> bigger_one;
         int len = nums.size();
-        std::vector<int> ret(len);
 
-        for (int i = 2*len - 1; i >= 0; --i) {
-            //std::cout << "i: " << i << std::endl;
-            while(!s.empty() && s.top() <= nums[i%len]) {
-                s.pop();
+        for (int i = 0; i <= 2*len - 1; ++i) {
+            while (!bigger_one.empty() && nums[bigger_one.top()] < nums[i%len]) {
+                ret[bigger_one.top()] = nums[i%len];
+                bigger_one.pop();
             }
-            ret[i%len] = s.empty() ? -1 : s.top();
-            s.push(nums[i%len]);
+
+            bigger_one.push(i%len);
         }
 
-        //std::cout << "ret.size " << ret.size() << std::endl;
         return ret;
     }
 };
 
 int main()
 {
-    //vector<int >nums = {1,2,3,4,3};
-    vector<int >nums = {1,2,1};
+    //vector<int >nums1 = {4,1,2};
+    //vector<int >nums1 = {2,4};
+    //vector<int >nums1 = {1,3,5,2,4};
+    //vector<int> nums2 = {1,3,4,2};
+    //vector<int> nums2 = {6,5,4,3,2,1,7};
+    //vector<int> nums = {1,2,3,4};
+    //vector<int> nums = {1,2,1};
+    vector<int> nums = {100,1,11,1,120,111,123,1,-1,-100};
     Solution *test = new Solution();
 
     for (const auto & item : test->nextGreaterElements(nums)) {
